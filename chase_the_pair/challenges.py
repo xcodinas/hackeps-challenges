1e37eda7703c083f17af56cf4f54a2ad43034de5
Xavier Codinas <xavier19966@gmail.com>
2019-10-29 13:36:30 +0100

    Improve algorithm to reduce time

--- chase_the_pair/challenges.py
+++ chase_the_pair/challenges.py
@@ -3,28 +3,16 @@ import time
 
 
 def close_pair(a, b, to_chase):
-    pairs = []
-    for index, item in enumerate(a):
-        if to_chase >= item:
-            close_upper = to_chase - item
-        elif to_chase <= item:
-            close_upper = item - to_chase
-
-        if to_chase >= b[index]:
-            close_lower = to_chase - b[index]
-        elif to_chase <= b[index]:
-            close_lower = b[index] - to_chase
-
-        pairs.append({
-            'upper': item,
-            'lower': b[index],
-            'close_upper': close_upper,
-            'close_lower': close_lower,
-            'sum': close_upper + close_lower,
-            })
-
+    pairs = [
+        {
+            'index': i,
+            'sum': (
+                to_chase - item if to_chase >= item else item - to_chase) + (
+                    to_chase - b[i] if to_chase >= item else item - b[i])
+            } for i, item in enumerate(a)
+        ]
     pairs.sort(key=lambda x: x.get('sum'))
-    return [pairs[0].get('upper'), pairs[0].get('lower')]
+    return [a[pairs[0].get('index')], b[pairs[0].get('index')]]
 
 
 def generate_sets(size, number=2):
