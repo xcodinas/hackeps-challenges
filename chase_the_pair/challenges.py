554d375896b1acb975d131496f8850696ab155d7
Xavier Codinas <xavier19966@gmail.com>
2019-10-29 16:59:02 +0100

    Add get_chase function

--- chase_the_pair/challenges.py
+++ chase_the_pair/challenges.py
@@ -22,12 +22,15 @@ def parse_sets(filename):
             [int(b) for b in data.split('\t')[1][2:-2].split(',')])
 
 
+def get_chase(a, b):
+    return random.randint(min(min(a), min(b)), max(max(a), max(b)))
+
+
 if __name__ == '__main__':
     a, b = parse_sets('logs.txt')
-    chase_number = random.randint(1, max(a))
+    chase_number = get_chase(a, b)
     start_time = time.process_time()
     print("------------RESULTS SECTION----------")
     print("Number to chase: %s" % chase_number)
     print("Result: %s" % close_pair(a, b, chase_number))
     print("--- %s seconds ---" % (time.process_time() - start_time))
-    test = time.process_time() - start_time
